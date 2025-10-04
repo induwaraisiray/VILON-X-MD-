8831b6452fbe35b03722069b31933aa797a51d27
Imduwara <induwaraisira827@gmail.com>
2025-10-04 11:19:25 +0530

    Update config.js

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "NIMA~MD&dmU0wDpB#3aMXYK5hyCMpF8nRLEKP8I0Dl95OQ3yZeJCxly4eTK4",
+SESSION_ID: process.env.SESSION_ID || "NIMA~MD&cMccMcIccc2EFzQ6A#XDNCg1A-4U7x99RUkfPwEj5_iEAmC1xIQaQq9Ju-SEc",
 AUTO_STATUS_SEEN: process.env.AUTO_STATUS_SEEN || "true",
 AUTO_STATUS_REPLY: process.env.AUTO_STATUS_REPLY || "false",
 AUTO_STATUS_REACT: process.env.AUTO_STATUS_REACT || "true",
